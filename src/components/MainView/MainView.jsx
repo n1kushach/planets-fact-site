067a3eaeb9a79c2111ccdef17866bf0166a03297
Nikolozi <nikolozi.tchavtchavadze.1@btu.edu.ge>
2023-03-13 13:12:43 +0400

    added mercury page

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 export const MainView = () => {
+
+  const navigate = useNavigate();
 
   return (
     <div className="w-screen h-screen bg-stars">
@@ -12,28 +14,28 @@ export const MainView = () => {
             THE PLANETS
           </h1>
           <div className="flex items-center gap-[33px]">
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Mercury py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/mercury")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Mercury py-2 text-[11px] font-LeagueSpartan font-bold">
               Mercury
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Venus py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/venus")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Venus py-2 text-[11px] font-LeagueSpartan font-bold">
               Venus
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Earth py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/earth")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Earth py-2 text-[11px] font-LeagueSpartan font-bold">
               Earth
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Mars py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/mars")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Mars py-2 text-[11px] font-LeagueSpartan font-bold">
               Mars
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Jupiter py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/jupiter")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Jupiter py-2 text-[11px] font-LeagueSpartan font-bold">
               Jupiter
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Saturn py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/saturn")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Saturn py-2 text-[11px] font-LeagueSpartan font-bold">
               Saturn
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Uranus py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/uranus")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Uranus py-2 text-[11px] font-LeagueSpartan font-bold">
               Uranus
             </h1>
-            <h1 className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Neptune py-2 text-[11px] font-LeagueSpartan font-bold">
+            <h1 onClick={() => navigate("/neptune")} className="text-[#FFFFFF] opacity-75 hover:opacity-100 uppercase cursor-pointer hover:border-t-2 hover:border-Neptune py-2 text-[11px] font-LeagueSpartan font-bold">
               Neptune
             </h1>
           </div>
--- src/components/Mercury/Mercury.jsx
+++ src/components/Mercury/Mercury.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import PlanetMercury from "../../assets/planet-mercury.svg";
 
 export const Mercury = () => {
   return (
-    <div>
-        <h1 className='text-[#FFFFFF]'>Mercury</h1>
+    <div className="flex flex-col w-[80%] mx-auto">
+      <div className="flex items-center justify-center gap-72 mt-10">
+        <img src={PlanetMercury} />
+        <div className="flex flex-col gap-5">
+          <h1 className="font-Antonio text-[80px] text-[#FFFFFF]">Mercury</h1>
+          <p className="font-light text-[14px] text-[#FFFFFF] max-w-[350px]">
+            Mercury is the smallest planet in the Solar System and the closest
+            to the Sun. Its orbit around the Sun takes 87.97 Earth days, the
+            shortest of all the Sun's planets. Mercury is one of four
+            terrestrial planets in the Solar System, and is a rocky body like
+            Earth.
+          </p>
+          <p className="font-LeagueSpartan font-light text-[14px] opacity-50 text-[#FFFFFF]">Source : <strong>Wikipedia</strong></p>
+          <div className="flex cursor-pointer hover:bg-active duration-300 items-center gap-[28px] border border-[#FFFFFF] p-2">
+            <p className="text-[#FFFFFF] text-[12px] font-bold opacity-50 ml-2">01</p>
+            <h2 className="text-[#FFFFFF] text-[12px] font-bold uppercase tracking-[2.57143px]">overview</h2>
+          </div>
+          <div className="flex cursor-pointer hover:bg-active duration-300 items-center gap-[28px] border border-[#FFFFFF] p-2">
+            <p className="text-[#FFFFFF] text-[12px] font-bold opacity-50 ml-2">02</p>
+            <h2 className="text-[#FFFFFF] text-[12px] font-bold uppercase tracking-[2.57143px]">internal structure</h2>
+          </div>
+          <div className="flex cursor-pointer hover:bg-active duration-300 items-center gap-[28px] border border-[#FFFFFF] p-2">
+            <p className="text-[#FFFFFF] text-[12px] font-bold opacity-50 ml-2">03</p>
+            <h2 className="text-[#FFFFFF] text-[12px] font-bold uppercase tracking-[2.57143px]">surface geology</h2>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center w-[90%] gap-4 mx-auto justify-between mt-12">
+        <div className="border border-[#9a9a9a] w-[30%] p-4">
+          <p className="uppercase text-[11px] text-[#FFFFFF] opacity-50">rotation time</p>
+          <h1 className="text-[40px] text-[#FFFFFF]">58.6 DAYS</h1>
+        </div>
+        <div className="border border-[#9a9a9a] w-[30%] p-4">
+          <p className="uppercase text-[11px] text-[#FFFFFF] opacity-50">revolution time</p>
+          <h1 className="text-[40px] text-[#FFFFFF]">87.96 DAYS</h1>
+        </div>
+        <div className="border border-[#9a9a9a] w-[30%] p-4">
+          <p className="uppercase text-[11px] text-[#FFFFFF] opacity-50">radius</p>
+          <h1 className="text-[40px] text-[#FFFFFF]">2,438.7 KM</h1>
+        </div>
+        <div className="border border-[#9a9a9a] w-[30%] p-4">
+          <p className="uppercase text-[11px] text-[#FFFFFF] opacity-50">average temp</p>
+          <h1 className="text-[40px] text-[#FFFFFF]">430°c</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
